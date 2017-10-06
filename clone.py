6bd5f1546797999354fe938ec63125e5fdd98409
BuildTools <benjibobs@users.noreply.github.com>
2017-10-06 18:47:38 +0100

    Resource downloading

--- clone.py
+++ clone.py
@@ -2,24 +2,70 @@
 import urllib2
 import sys
 import socket
+import os
 
-socket.setdefaulttimeout(10)
+socket.setdefaulttimeout(15)
+
+dataTypesToDownload = [".jpg", ".png", ".ico", ".css", ".js"]
 
 if len(sys.argv) == 1:
 	url = raw_input("URL of site to clone: ")
 else:
 	url = sys.argv[1]
 
-if "http://" not in url:
+if "http://" not in url and "https://" not in url:
 	url = "http://"+url
 
-
-file = open("index.html", "w")
+try:
+	os.mkdir("clone")
+except OSError:
+	pass
+file = open("clone/index.html", "w")
 try:
 	content = urllib2.urlopen(url).read()
 except urllib2.URLError as e:
 	print "An error occured: " + str(e.reason)
 	exit()
+
+resources = content.split("=\"")
+
+first = False
+for resource in resources:
+	if first == False:
+		first = True
+		continue
+	resource = resource.split("\"")[0]
+	if any(s in resource for s in dataTypesToDownload):
+		print "Downloading " + resource
+		try:
+			path = resource.split("/")
+			
+			if len(path) != 1:
+				path.pop(len(path) - 1)
+				trail = "./clone/"
+				for folder in path:
+					trail += folder+"/"
+					try:
+						os.mkdir(trail)
+					except OSError:
+						pass	
+		except IOError:
+			pass
+		try:
+			download = open("clone/"+resource, "w")
+			print url+"/"+resource
+			dContent = urllib2.urlopen(url+"/"+resource).read()
+		except urllib2.URLError as e:
+			print "An error occured: " + str(e.reason)
+			download.close()
+			continue
+		except IOError:
+			pass
+			continue
+		download.write(dContent)
+		download.close()
+		print "Downloaded!"
+
 
 content = content.replace("=\"/", "=\""+url+"/")
 
