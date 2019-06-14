4600f3c31ea69d36a1b3f96e0071241968d732fd
benjibobs <benjibobs@users.noreply.github.com>
2019-06-14 09:55:23 +0100

    Add experimental support for url(x) in CSS

--- clone.py
+++ clone.py
@@ -4,6 +4,59 @@ import socket
 import os
 import re
 
+def download(resource):
+
+	if any(s in resource for s in dataTypesToDownload):
+
+		if " " in resource: # https://stackoverflow.com/a/4172592
+			return
+
+		while resource.startswith("/"):
+			resource = resource[1:]
+
+		if resource.startswith("https://") or resource.startswith("http://"):
+			resource = resource.replace("https://", "").replace("http://", "")
+
+		if resource.startswith("../"):
+			resource = resource.replace("../", "dotdot/")
+
+		try:
+			path = resource.split("/")
+			
+			if len(path) != 1:
+				path.pop(len(path) - 1)
+				trail = "./" + base_path + "/"
+				for folder in path:
+					trail += folder+"/"
+					try:
+						os.mkdir(trail)
+					except OSError:
+						pass	
+
+		except IOError:
+			pass
+
+		try:
+
+			if "?" in resource:
+				download = open(base_path + "/"+ resource.split("?")[len(resource.split("?")) - 2], "w")
+			else:
+				download = open(base_path + "/"+ resource, "w")
+
+			print("Downloading {} to {}".format(resource, download.name))
+
+			dContent = requests.get(url+"/"+resource).text
+		
+		except Exception as e:
+		
+			print("An error occured: " + str(e.reason))
+			download.close()
+			return
+		
+		download.write(dContent)
+		download.close()
+		print("Downloaded!")
+
 socket.setdefaulttimeout(15)
 
 dataTypesToDownload = [".jpg", ".jpeg", ".png", ".gif", ".ico", ".css", ".js", ".html"]
@@ -46,54 +99,24 @@ for resource in resources:
 
 	resource = re.split("\"|'", resource)[0]
 
-	if any(s in resource for s in dataTypesToDownload):
+	download(resource)
 
-		while resource.startswith("/"):
-			resource = resource[1:]
-
-		if resource.startswith("https://") or resource.startswith("http://"):
-			resource = resource.replace("https://", "").replace("http://", "")
-
-		if resource.startswith("../"):
-			resource = resource.replace("../", "dotdot/")
-
-		try:
-			path = resource.split("/")
-			
-			if len(path) != 1:
-				path.pop(len(path) - 1)
-				trail = "./" + base_path + "/"
-				for folder in path:
-					trail += folder+"/"
-					try:
-						os.mkdir(trail)
-					except OSError:
-						pass	
-
-		except IOError:
-			pass
-
-		try:
-
-			
-
-			if "?" in resource:
-				download = open(base_path + "/"+ resource.split("?")[len(resource.split("?")) - 2], "w")
-			else:
-				download = open(base_path + "/"+ resource, "w")
-
-			print("Downloading {} to {}".format(resource, download.name))
-
-			dContent = requests.get(url+"/"+resource).text
+for subdir, dirs, files in os.walk(base_path):
+	for file in files:
 		
-		except Exception as e:
-		
-			print("An error occured: " + str(e.reason))
-			download.close()
+		if file == ".DS_Store":
 			continue
 		
-		download.write(dContent)
-		download.close()
-		print("Downloaded!")
-
+		f = open(os.path.join(subdir, file), 'r')
+		
+		content = f.read()
+		if "url(" in content:
+			arr = content.split("url(")
+			iterations = len(arr) - 1
+			i = 1
+			for x in range(iterations):
+				path = arr[i].split(")")[0]
+				download(url+"/"+path)
+				i += 1
+			
 print("Cloned "+url+" !")
