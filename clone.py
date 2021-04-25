57cb06be4f03ebab8a2d704c97fe3f9baa83365b
souparno <souparno.majumder@gmail.com>
2021-04-25 21:34:32 +0530

    beuatiful soup is unnecessary

--- clone.py
+++ clone.py
@@ -5,8 +5,21 @@ import re
 import socks
 import socket
 from urllib.request import Request, urlopen
-from bs4 import BeautifulSoup
 
+
+def get(url):
+    IP_ADDR = '127.0.0.1'
+    PORT = 9050
+
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+
+    request = Request(url)
+    socks.set_default_proxy(socks.SOCKS5, IP_ADDR, PORT)
+    socket.socket = socks.socksocket
+
+    return  urlopen(request, context=ctx)
 
 #  build the path by removing extra // and resolving relative path
 #  ex: abc//def = abc/def
@@ -23,19 +36,15 @@ def build_path(path):
 
     return temp_path
 
-def get(url):
-    IP_ADDR = '127.0.0.1'
-    PORT = 9050
+def resources(content):
+    items = []
+    regex = "a-zA-Z0-9\.\/:-"
+    regex = "([" + regex + "]+)" + str("(" + "|".join(dataTypesToDownload) + ")").replace(".", "\.") + "([" + regex + "?=&]*)"
 
-    ctx = ssl.create_default_context()
-    ctx.check_hostname = False
-    ctx.verify_mode = ssl.CERT_NONE
+    for match in re.findall(regex, content):
+       items.append(''.join(match))
 
-    request = Request(url)
-    socks.set_default_proxy(socks.SOCKS5, IP_ADDR, PORT)
-    socket.socket = socks.socksocket
-
-    return  urlopen(request, context=ctx)
+    return items
 
 def download(item):
 
@@ -45,103 +54,109 @@ def download(item):
     global domain
     global url
 
-    if any(s in item for s in dataTypesToDownload):
+    #  if any(s in item for s in dataTypesToDownload):
 
-        if " " in item:  # https://stackoverflow.com/a/4172592
-            return
+    if " " in item:  # https://stackoverflow.com/a/4172592
+        return
 
-        while item.startswith("/"):
-            item = item[1:]
+    while item.startswith("/"):
+        item = item[1:]
 
-        external = False
-        prefix = ""
+    external = False
+    prefix = ""
 
-        if "#" in item:
-            item = item.split("#")[0]
+    if "#" in item:
+        item = item.split("#")[0]
 
-        if item.startswith(url):
-            external = True
-            prefix = url
-            item = item.replace(url, "")
+    if item.startswith(url):
+        external = True
+        prefix = url
+        item = item.replace(url, "")
 
-        if item.startswith(domain):
-            external = True
-            prefix = domain
-            item = item.replace(domain, "")
+    if item.startswith(domain):
+        external = True
+        prefix = domain
+        item = item.replace(domain, "")
 
-        if item.startswith("https://"):
-            external = True
-            prefix = "https://"
-            item = item.replace("https://", "")
-                
-        if item.startswith("http://"):
-            external = True
-            prefix = "http://"
-            item = item.replace("http://", "")
+    if item.startswith("https://"):
+        external = True
+        prefix = "https://"
+        item = item.replace("https://", "")
 
-        if item.startswith("../"):
-            item = item.replace("../", "dotdot/")
+    if item.startswith("http://"):
+        external = True
+        prefix = "http://"
+        item = item.replace("http://", "")
 
-        if item in downloadedFiles:
-            return
+    if item.startswith("../"):
+        item = item.replace("../", "dotdot/")
 
-        try:
-            item_path = item.split("/")
-            
-            if len(item_path) != 1:
-                item_path.pop(len(item_path) - 1)
-                trail = "./" + base_path + "/"
-                for folder in item_path:
-                    trail += folder+"/"
-                    try:
-                            os.mkdir(trail)
-                    except OSError:
-                            pass	
-
-        except IOError:
-            pass
-
-        try:
-            if "?" in item:
-                download_path = build_path("/" + item.split("?")[len(item.split("?")) - 2])
-            else:
-                download_path = build_path("/" + item)
-
-            download = open(build_path(base_path + download_path), "wb")
-            downloadPaths.append(download_path)
-           
-            
-            if external:
-                d_url = build_path(prefix + item)
-            else:
-                d_url = build_path(url + "/" + item)
-
-            #  adding the / to http:/ or https:/ that got removed while build_path
-            protocol = re.findall("http:\/|https:\/",  d_url)
-            d_url = re.sub("http:\/|https:\/", protocol[0] + "/", d_url)
-           
-            print("Downloading {} to {}".format(d_url, download.name))
-            dContent = get(d_url)
-        except Exception as e:
- 
-            print("An error occured: " + str(e.reason))
-            download.close()
-            return
+    try:
+        item_path = item.split("/")
         
-        for chunk in dContent:
-            download.write(chunk)
+        if len(item_path) != 1:
+            item_path.pop(len(item_path) - 1)
+            trail = "./" + base_path + "/"
+            for folder in item_path:
+                trail += folder+"/"
+                try:
+                        os.mkdir(trail)
+                except OSError:
+                        pass	
 
+    except IOError:
+        pass
+
+    try:
+        if "?" in item:
+            download_path = build_path("/" + item.split("?")[len(item.split("?")) - 2])
+        else:
+            download_path = build_path("/" + item)
+
+        #  If the element is already downloaded make downloaded flag true  and move to the 
+        #  end of the list so that the content can be overwritten with the correct path
+        #  if download_path in downloadPaths:
+        #      print("======= H E Y   C H E C K   T H I S   O U T =====")
+        #      downloaded = True
+        #      downloadPaths.pop(downloadPaths.index(download_path))
+        #      return
+
+        download = open(build_path(base_path + download_path), "wb")
+        downloadPaths.append(download_path)
+ 
+        if external:
+            d_url = build_path(prefix + item)
+        else:
+            d_url = build_path(url + "/" + item)
+
+        #  adding the / to http:/ or https:/ that got removed while build_path
+        #  for protocol in re.findall("http:\/|https:\/",  d_url):
+        #      d_url = re.sub("http:\/|https:\/", protocol + "/", d_url)       
+        d_url = re.match("(http:\/|https:\/)(.+)", d_url)
+        d_url = d_url.group(1) + "/" + d_url.group(2)
+
+        print("Downloading {} to {}".format(d_url, download.name))
+        dContent = get(d_url)
+    except Exception as e:
+
+        print("An error occured: " + str(e.reason))
         download.close()
-        downloadedFiles.append(resource)
-        downloaded = True
-        print("Downloaded!")
+        return
+    
+    for chunk in dContent:
+        download.write(chunk)
 
+    download.close()
+    #  downloadedFiles.append(resource)
+    downloaded = True
+    print("Downloaded!")
 
 
 downloadedFiles = []
 downloadPaths = []
 downloaded = False
 dataTypesToDownload = [".svg", ".jpg", ".jpeg", ".png", ".gif", ".ico", ".css", ".js", ".html", ".php", ".json", ".ttf", ".otf", ".woff", ".woff2", ".eot", ".mp4"]
+textFiles = ["css", "js", "html", "php", "json"]
 
 if len(sys.argv) == 1:
     url = input("URL of site to clone: ")
@@ -170,50 +185,28 @@ except OSError:
 
 response = get(url)
 content = response.read().decode('utf-8')
-soup = BeautifulSoup(content, "html.parser")
 
-
-#  Downloading all the css ref in the link tag
-for link in soup.find_all('link', href=True):
-    resource = link['href']
-    download(resource)
+for resource in resources(content):
+    download(resource) 
 
     if downloaded == True:
-        print("link href attribute modified !")
-
-        link['href'] = downloadPaths[-1]
+        content = content.replace(resource, downloadPaths[-1])
         downloaded = False
 
-
-#  Downloading all the js in the script src attribute
-for script in soup.find_all('script', src=True):
-    resource = script['src']
-    download(resource)
-
-    if downloaded == True:
-        print("script src attribute modified !")
-
-        script['src'] = downloadPaths[-1]
-        downloaded = False
+        #  print("==========================")
+        #  print("replacing " + resource + " with " + downloadPaths[-1])
+        #  print("==========================")
 
 
-#  Download all the image src links to local
-for img in soup.find_all('img', src=True):
-    resource = img['src']
-    download(resource)
-
-    if downloaded == True:
-        print("img src attribute modified !")
-
-        img['src'] = downloadPaths[-1]
-        downloaded = False
 
 file = open(build_path(base_path + "/index.html"), "w")
-file.write(str(soup))
+file.write(content)
 file.close()
 
+    #  if resource == "/wp-content/themes/twentysixteen/images/newdesign/appfutura.svg":
+    #      break
 
-textFiles = ["css", "js", "html", "php", "json"]
+
 print('Scanning for CSS based url(x) references...')
 
 for subdir, dirs, files in os.walk(base_path):
@@ -252,4 +245,49 @@ for subdir, dirs, files in os.walk(base_path):
         f.close()
 
 
-print("Cloned "+url+" !")
+#  def get_listing(path):
+#      file_listing = []
+
+#      for subdir, dirs, files in os.walk(path):
+#          for f in files:
+#              file_listing.append(os.path.join(subdir, f))
+
+#      return file_listing
+
+#  before = []
+#  after = get_listing(base_path)
+#  diff = [f for f in after if not f in before]
+
+#  while diff:
+    
+#      for file in diff:
+
+#          if file.split(".")[-1]  == "DS_Store" or file.split(".")[-1] not in textFiles:
+#              continue
+
+#          f = open(file, 'r+')
+
+#          print("S C A N N I N G   F I L E : " + f.name)
+      
+
+#          content = f.read()
+
+#          for resource in resources(content):
+#              download(resource) 
+        
+#              if downloaded == True:
+#                  content = content.replace(resource, downloadPaths[-1])
+#                  downloaded = False
+            
+
+#          f.seek(0)
+#          f.truncate()
+#          f.write(content)
+#          f.close()
+
+#      before = after
+#      after = get_listing(base_path)
+#      diff = [f for f in after if not f in before]
+
+
+print("Cloned " + url + " !")
